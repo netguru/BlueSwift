af108ff85788de01a9aaf36d7cea855f0fd10275
poszposz <jan.posz@netguru.pl>
2018-01-10 16:39:11 +0100

    added tests for array extension

--- Framework/Unit Tests/ExtensionTests.swift
+++ Framework/Unit Tests/ExtensionTests.swift
@@ -7,9 +7,12 @@
 //
 
 import XCTest
+import CoreBluetooth
 @testable import Bluetooth
 
 class ExtensionTests: XCTestCase {
+    
+    //MARK: String extension tests
     
     func testShortUUIDValidation() {
         let fourCharactersCorrect = "1800"
@@ -20,20 +23,49 @@ class ExtensionTests: XCTestCase {
         
         let notHexadecimal = "180X"
         
-        XCTAssertTrue(fourCharactersCorrect.isValidShortenedUUID())
-        XCTAssertTrue(sixCharactersCorrext.isValidShortenedUUID())
-        XCTAssertFalse(tooLong.isValidShortenedUUID())
-        XCTAssertFalse(tooShort.isValidShortenedUUID())
-        XCTAssertFalse(notHexadecimal.isValidShortenedUUID())
+        XCTAssertTrue(fourCharactersCorrect.isValidShortenedUUID(), "Four character valid uuid validation failed")
+        XCTAssertTrue(sixCharactersCorrext.isValidShortenedUUID(), "Six character valid uuid validation failed")
+        XCTAssertFalse(tooLong.isValidShortenedUUID(), "Too long invalid uuid validation passed")
+        XCTAssertFalse(tooShort.isValidShortenedUUID(), "Too short invalid uuid validation passed")
+        XCTAssertFalse(notHexadecimal.isValidShortenedUUID(), "Not hexadecimal invalid uuid validation passed")
     }
     
+    //MARK: Array extension tests
+    
     func testArrayCommonPart() {
-        
         let first = [1, 2, 3, 4, 5]
         let second = [4, 5, 6, 7]
         
-        let common = first.matchingElementsWith(second) { (firstElement, <#T#>) -> Bool in
-            <#code#>
-        }
+        let common = first.matchingElementsWith(second) { $0 == $1 }
+        
+        XCTAssertEqual(common.count, 2, "Expected matches count is invalid")
+        XCTAssertEqual(common.first?.0, 4, "Array first element matching failed")
+        XCTAssertEqual(common.last?.1, 5, "Array second element matching failed")
+    }
+    
+    func testServiceMatching() {
+        
+        let services = [try! Service(uuid: "1800", characteristics: []),
+                        try! Service(uuid: "1801", characteristics: [])]
+        let cbServices = [CBMutableService(type: CBUUID(string: "1800"), primary: false) as CBService,
+                          CBMutableService(type: CBUUID(string: "1802"), primary: false) as CBService]
+        
+        let common = services.matchingElementsWith(cbServices)
+        
+        XCTAssertEqual(common.count, 1, "Expected matches count is invalid")
+        XCTAssertEqual(common.first?.0.bluetoothUUID.uuidString, common.first?.1.uuid.uuidString, "Expected uuid's does not match")
+    }
+    
+    func testCharacteristicMatching() {
+        
+        let services = [try! Characteristic(uuid: "2A0B"),
+                        try! Characteristic(uuid: "1801")]
+        let cbCharacteristics = [CBMutableCharacteristic(type: CBUUID(string: "1801"), properties: [.read], value: nil, permissions: .readable) as CBCharacteristic,
+                          CBMutableCharacteristic(type: CBUUID(string: "1800"), properties: [.read], value: nil, permissions: .readable) as CBCharacteristic]
+        
+        let common = services.matchingElementsWith(cbCharacteristics)
+        
+        XCTAssertEqual(common.count, 1, "Expected matches count is invalid")
+        XCTAssertEqual(common.first?.0.bluetoothUUID.uuidString, common.first?.1.uuid.uuidString, "Expected uuid's does not match")
     }
 }
