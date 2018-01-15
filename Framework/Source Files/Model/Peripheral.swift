e163a487aad25b81facb9fbfdcb3b3bed50da876
poszposz <jan.posz@netguru.pl>
2018-01-15 20:24:04 +0100

    additional documentation

--- Framework/Source Files/Model/Peripheral.swift
+++ Framework/Source Files/Model/Peripheral.swift
@@ -10,6 +10,7 @@ import CoreBluetooth
 /// Configuration. It presents a clear interface for writing and reading interactions with remote peripherals adding closure reponses.
 public final class Peripheral: NSObject {
     
+    /// List of errors possible to happen upon a write request.
     public enum WriteError: Error {
         case characteristicNotWriteable
         case characteristicNotDiscovered
@@ -17,6 +18,7 @@ public final class Peripheral: NSObject {
         case writeError(Error)
     }
     
+    /// List of errors possible to happen upon a read request.
     public enum ReadError: Error {
         case characteristicNotDiscovered
         case characteristicNotReadable
@@ -41,6 +43,7 @@ public final class Peripheral: NSObject {
     /// - Parameter configuration: proviously created configuration containing all desired services and characteristics.
     /// - Parameter deviceIdentifier: optional parameter. If device identifier is cached locally than it should be passed here.
     ///   When set, connection to peripheral is much quicker.
+    /// - SeeAlso: Configuration
     public init(configuration: Configuration, deviceIdentifier: String? = nil) {
         self.configuration = configuration
         self.deviceIdentifier = deviceIdentifier
@@ -53,13 +56,22 @@ public final class Peripheral: NSObject {
         }
     }
     
+    /// Private variable for storing reference to write completion callback.
     private var writeHandler: ((WriteError?) -> ())?
     
+    /// Private variable for storing reference to read completion callback.
     private var readHandler: ((Data?, ReadError?) -> ())?
 }
 
 public extension Peripheral {
     
+    /// Method used for writing to the peripheral after it's connected.
+    /// - Parameter command: a command to write to the device.
+    /// - Parameter characteristic: a characteristic the command should be directed to.
+    /// - Parameter handler: a completion handler indicating if reuqest was succesfull.
+    /// - SeeAlso: Command
+    /// - SeeAlso: Characteristic
+    /// - SeeAlso: Peripheral.WriteError
     public func write(command: Command, characteristic: Characteristic, handler: @escaping (WriteError?) -> ()) {
         writeHandler = handler
         guard isConnected else {
@@ -77,6 +89,11 @@ public extension Peripheral {
         peripheral?.writeValue(command.data, for: characteristic, type: .withResponse)
     }
     
+    /// Method used to perform read request from peripheral after it's connected.
+    /// - Parameter characteristic: a characteristic you wish to read.
+    /// - Parameter handler: completion handler returning Data retrieved from characteristic or error if it failed.
+    /// - SeeAlso: Characteristic
+    /// - SeeAlso: Peripheral.ReadError
     public func read(_ characteristic: Characteristic, handler: @escaping (Data?, ReadError?) -> ()) {
         readHandler = handler
         guard isConnected else {
@@ -97,6 +114,8 @@ public extension Peripheral {
 
 extension Peripheral: CBPeripheralDelegate {
     
+    /// Called after reading data from characteristic.
+    /// - SeeAlso: CBPeripheralDelegate
     public func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
         guard let handler = writeHandler else { return }
         guard let error = error else {
@@ -106,8 +125,12 @@ extension Peripheral: CBPeripheralDelegate {
         handler(.writeError(error))
     }
     
+    /// Called in two cases:
+    /// 1) After performing read request from peripheral.
+    /// 2) After peripheral updates value for characteristic with notify turned on.
+    /// - SeeAlso: CBPeripheralDelegate
     public func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-        // It's assumed that if someone performed a read request, we'll ignore calling a notification for this value
+        // It's assumed that if someone performed a read request, we'll ignore calling a notification for this value.
         if let handler = readHandler {
             handler(characteristic.value, nil)
             return
