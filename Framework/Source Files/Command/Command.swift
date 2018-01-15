44850c24a8a9dc8746265aaac35371d52e1c1bec
poszposz <jan.posz@netguru.pl>
2018-01-15 20:31:44 +0100

    command enum documentation

--- Framework/Source Files/Command/Command.swift
+++ Framework/Source Files/Command/Command.swift
@@ -6,6 +6,10 @@
 
 import Foundation
 
+/// Command enum - a handy wrapper for creating requests to peripheral devices.
+/// Handles creation of data with length according to passed parameter type, also creates data straight from hexadecimal
+/// string with exact the same value. If none of the cases matches needed type, use .data(Data) case where Data object can be
+/// passed directly.
 public enum Command {
     case int8(UInt8)
     case int16(UInt16)
@@ -16,6 +20,7 @@ public enum Command {
 
 internal extension Command {
     
+    /// Variable used for conversion of parameters to Data possible to write to peripheral.
     internal var data: Data {
         return Data()
     }
