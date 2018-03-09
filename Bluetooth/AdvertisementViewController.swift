a441b4fd158ad02f78a862608212f3a8638ad792
poszposz <jan.posz@netguru.pl>
2018-03-09 15:42:29 +0100

    added storyboard for easier sample app reading

--- Bluetooth/AdvertisementViewController.swift
+++ Bluetooth/AdvertisementViewController.swift
@@ -7,77 +7,34 @@ import UIKit
 import Bluetooth
 
 class AdvertisementViewController: UIViewController {
+    
+    @IBOutlet weak var notifyTextField: UITextField!
+    @IBOutlet weak var readTextField: UITextField!
+    @IBOutlet weak var textView: UITextView!
 
     let advertisement = BluetoothAdvertisement.shared
     let characteristic = try! Characteristic(uuid: "00001002-4554-2049-4e43-2e205555726f")
     let otherCharacteristic = try! Characteristic(uuid: "00001003-4554-2049-4e43-2e205555726f")
     
-    private lazy var advertiseButton: UIButton = {
-        let button = UIButton(type: .custom)
-        button.setTitleColor(UIColor.black, for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Start advertising", for: .normal)
-        button.addTarget(self, action: #selector(advertise), for: .touchUpInside)
-        return button
-    }()
-    
-    private lazy var updateButton: UIButton = {
-        let button = UIButton(type: .custom)
-        button.setTitleColor(UIColor.black, for: .normal)
-        button.setTitle("Update data", for: .normal)
-        button.addTarget(self, action: #selector(update), for: .touchUpInside)
-        return button
-    }()
-    
-    private lazy var textField: UITextField = {
-        let textField = UITextField()
-        textField.borderStyle = .bezel
-        return textField
-    }()
-    
-    private lazy var textView = UITextView()
-    
-    private lazy var stackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [textField, updateButton, advertiseButton, textView])
-        stackView.axis = .vertical
-        stackView.spacing = 30
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        textView.textColor = UIColor.black
-        view.backgroundColor = UIColor.white
-        view.addSubview(stackView)
-        let constraints = [stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-                           stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                           textView.heightAnchor.constraint(equalToConstant: 300),
-                           textView.widthAnchor.constraint(equalToConstant: 300)]
-        NSLayoutConstraint.activate(constraints)
-        let tap = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
-        view.addGestureRecognizer(tap)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        advertisement.stopAdvertising()
-    }
-    
-    @objc func hideKeyboard() {
-        view.endEditing(true)
-    }
-    
-    @objc func advertise() {
+    @IBAction func advertise() {
         let service = try! Service(uuid: "00001001-4554-2049-4e43-2e205555726f", characteristics: [characteristic, otherCharacteristic])
         let configuration = try! Configuration(services: [service], advertisement: "00001001-4554-2049-4e43-2e205555726f")
-        
         let peripheral = Peripheral(configuration: configuration, advertisementData: [.localName("Test"), .servicesUUIDs("00001001-4554-2049-4e43-2e205555726f")])
         
         advertisement.advertise(peripheral: peripheral) { _ in
             print("Error during adverisement")
         }
-        
+        handleRequests()
+    }
+    
+    @IBAction func update() {
+        let command = Command.utf8String(notifyTextField.text!)
+        advertisement.update(command, characteristic: characteristic) { error in
+            print("Notified on central.")
+        }
+    }
+    
+    func handleRequests() {
         advertisement.writeRequestCallback = { [weak self] characteristic, data in
             guard var text = self?.textView.text, let data = data else { return }
             text = text + "\nWrote: " + (String(data: data, encoding: .utf8) ?? "(Error encoding.)") + ". Characteristic: " + characteristic.uuid
@@ -85,17 +42,19 @@ class AdvertisementViewController: UIViewController {
         }
         
         advertisement.readRequestCallback = { [weak self] characteristic -> Data in
-            guard var text = self?.textView.text else { return Data() }
+            guard var text = self?.textView.text, let notifyValue = self?.readTextField.text else { return Data() }
             text = text + "\nReceived read request. Characteristic: " + characteristic.uuid
             self?.textView.text = text
-            return "Hello world".data(using: .utf8, allowLossyConversion: false)!
+            return notifyValue.data(using: .utf8, allowLossyConversion: false)!
         }
     }
     
-    @objc func update() {
-        let command = Command.utf8String(textField.text!)
-        advertisement.update(command, characteristic: characteristic) { error in
-            print("Updated")
-        }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        advertisement.stopAdvertising()
+    }
+    
+    @IBAction func hideKeyboard() {
+        view.endEditing(true)
     }
 }
